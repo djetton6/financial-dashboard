9d4087de38c4fade6f268f57100af44971b5901a
Desmend Jetton <desmendjetton@gmail.com>
2020-07-11 19:30:16 -0500

    updated names of Nav and imported

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'office-ui-fabric-react/dist/css/fabric.css';
+import Navigation from './Navigation.js';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Initial Commit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ms-Grid" dir="ltr">
+      <div className="ms-Grid-row">
+      <div className="ms-Grid-col ms-sm-1 ms-xl1">
+        <Navigation />
+      
+      </div>
+      </div>
     </div>
   );
 }
